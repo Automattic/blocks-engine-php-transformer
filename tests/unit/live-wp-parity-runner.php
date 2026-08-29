7c6fba83e97d0bc1a07beb8f07756661af938a12
Chris Huber <chris.huber@automattic.com>
2026-08-29 15:20:23 -0400

    Model dynamic navigation in static parity

--- tests/unit/live-wp-parity-runner.php
+++ tests/unit/live-wp-parity-runner.php
@@ -121,6 +121,20 @@ $assert(
     '2b: render-free proxy materializes the stable structure of dynamic social-link children'
 );
 
+$navigationProxy = StaticStyleParityRunner::candidateHtmlFromSerializedBlocks(
+    '<!-- wp:navigation {"className":"main-nav"} --><!-- wp:navigation-submenu {"className":"main-nav__item","label":"Platform \\u003cspan class=\\u0022caret\\u0022\\u003e↓\\u003c/span\\u003e","url":"/platform"} --><!-- wp:navigation-link {"className":"mega-link","label":"Supplier 360\\u003cspan class=\\u0022description\\u0022\\u003eUnified record\\u003c/span\\u003e","url":"/platform#supplier"} /--><!-- /wp:navigation-submenu --><!-- /wp:navigation -->'
+);
+$assert(
+    str_contains($navigationProxy, '<nav class="wp-block-navigation main-nav"><ul class="wp-block-navigation__container wp-block-navigation main-nav">')
+        && str_contains($navigationProxy, '<li class="wp-block-navigation-item main-nav__item has-child">')
+        && str_contains($navigationProxy, '<a class="wp-block-navigation-item__content" href="/platform">')
+        && str_contains($navigationProxy, '<span class="caret">↓</span>')
+        && str_contains($navigationProxy, '<li class="wp-block-navigation-item mega-link">')
+        && str_contains($navigationProxy, '<span class="description">Unified record</span>')
+        && str_ends_with($navigationProxy, '</li></ul></li></ul></nav>'),
+    '2c: render-free proxy materializes nested dynamic navigation block structure'
+);
+
 // ---------------------------------------------------------------------------
 // 3. Faithful live render scores perfectly (parity == 1.0, no findings).
 // ---------------------------------------------------------------------------
